9121d88845d7734b321972afc2bf4fc71a924dae
kuma-24 <surei.mikurio2525@gmail.com>
2023-07-11 20:39:17 +0900

    環境構築

---
+++ dashboard/laravel/public/index.php
@@ -0,0 +1,29 @@
+<?php
+// defineの値は環境によって変えてください。
+define('HOSTNAME', 'database');
+define('DATABASE', 'laravel_database');
+define('USERNAME', 'laravel_uesr');
+define('PASSWORD', 'laravel_pass_secret');
+
+try {
+  /// DB接続を試みる
+  $db  = new PDO('mysql:host=' . HOSTNAME . ';dbname=' . DATABASE, USERNAME, PASSWORD);
+  $msg = "MySQL への接続確認が取れました。";
+} catch (PDOException $e) {
+  $isConnect = false;
+  $msg       = "MySQL への接続に失敗しました。<br>(" . $e->getMessage() . ")";
+}
+?>
+<html>
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>MySQL接続確認</title>
+</head>
+
+<body>
+  <h1>MySQL接続確認:dashboard</h1>
+  <p><?php echo $msg; ?></p>
+</body>
+
+</html>
