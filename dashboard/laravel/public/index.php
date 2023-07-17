0b490ef0bfb78ae9001f60c19e7e7a3fc2007906
kuma-24 <surei.mikurio2525@gmail.com>
2023-07-17 15:08:26 +0900

    フレームワーク追加

--- dashboard/laravel/public/index.php
+++ dashboard/laravel/public/index.php
@@ -1,29 +1,55 @@
 <?php
-// defineの値は環境によって変えてください。
-define('HOSTNAME', 'database');
-define('DATABASE', 'laravel_database');
-define('USERNAME', 'laravel_uesr');
-define('PASSWORD', 'laravel_pass_secret');
 
-try {
-  /// DB接続を試みる
-  $db  = new PDO('mysql:host=' . HOSTNAME . ';dbname=' . DATABASE, USERNAME, PASSWORD);
-  $msg = "MySQL への接続確認が取れました。";
-} catch (PDOException $e) {
-  $isConnect = false;
-  $msg       = "MySQL への接続に失敗しました。<br>(" . $e->getMessage() . ")";
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
-?>
-<html>
 
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>MySQL接続確認</title>
-</head>
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 
-<body>
-  <h1>MySQL接続確認:dashboardeee</h1>
-  <p><?php echo $msg; ?></p>
-</body>
+require __DIR__.'/../vendor/autoload.php';
 
-</html>
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
